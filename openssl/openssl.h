dc2439ee5a76661c8adf7ef4384636dd12b08e7e
oTranVuongMinh <tran.vuong.minh@framgia.com>
2017-06-08 16:18:12 +0700

    Init

---
+++ openssl/openssl.h
@@ -0,0 +1,19 @@
+//
+//  openssl.h
+//  openssl
+//
+//  Created by tran vuong minh on 6/8/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for openssl.
+FOUNDATION_EXPORT double opensslVersionNumber;
+
+//! Project version string for openssl.
+FOUNDATION_EXPORT const unsigned char opensslVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <openssl/PublicHeader.h>
+
+
